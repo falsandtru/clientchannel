d9517f099be0e93e53af32e7a05b0ffbe425ce0a
falsandtru <falsandtru@users.noreply.github.com>
2016-03-14 02:55:48 +0900

    docs(readme): add an usage section

--- README.md
+++ README.md
@@ -15,3 +15,85 @@ Communicate between other tabs via IndexedDB and LocalStorage.
 ## API
 
 [localsocket.d.ts](typings/localsocket.d.ts)
+
+## Usage
+
+### Persistence
+
+Schemas are defined by property names that made by factory function.
+A property name that has `_` prefix or postfix to be ignored.
+It means you can define the dynamic value object.
+
+Data that assigned to the property of Linked object will save to the storage.
+When data was updated on others threads(tabs), own property value will update automatically.
+
+```ts
+import {socket, LocalSocketObject} from 'localsocket';
+
+interface Schema extends LocalSocketObject {
+}
+class Schema {
+	// getter/setter will exclude in schema.
+	get key() {
+		return this.__key;
+	}
+	// property names that has underscore prefix or postfix will exclude in schema.
+	private _value = 0;
+	// basic property names will include in schema.
+	value = 1;
+	// invalid value types will exclude in schema.
+	join() {
+		return this._value + this.value;
+	}
+}
+
+const sock = socket('domain', {
+	// delete linked record 3 days later from last access.
+	expiry: 3 * 24 * 60 * 60 * 1e3,
+	factory() {
+		return new Schema();
+	}
+});
+// load data from indexeddb a little later.
+const link: Schema = sock.link('path');
+// save data to indexeddb, and sync data between all tabs.
+link.value = 1;
+```
+
+### Communicate and Synchronization
+
+Linked object provedes send/recv event.
+`send` event will emit when linked object was updated by own thread(tab).
+`recv` event will emit when linked object was updated by other threads(tabs).
+
+```ts
+import {port, LocalPortObject} from 'localsocket';
+
+interface Schema extends LocalPortObject {
+}
+class Schema {
+	get event() {
+		return this.__event;
+	}
+	version = 0;
+}
+
+const sock = port('version', {
+	life: 3,
+	factory() {
+		return new Schema();
+	}
+});
+const link: Schema = sock.link();
+const VERSION = 1;
+link.event.monitor('recv', ({newValue}) => {
+	if (newValue === VERSION) return;
+	if (newValue > VERSION) {
+		location.reload();
+	}
+	else {
+		link.version = VERSION;
+	}
+});
+link.version = VERSION;
+```
---
+++ test/integration/package.ts
@@ -0,0 +1,87 @@
+import {socket, LocalSocketObject} from 'localsocket';
+import {port, LocalPortObject} from 'localsocket';
+
+describe('Integration: Package', function () {
+  describe('usage', function () {
+    it('persistence', () => {
+      interface Schema extends LocalSocketObject {
+      }
+      class Schema {
+        // getter/setter will exclude in schema.
+        get key() {
+          return this.__key;
+        }
+        // property names that has underscore prefix or postfix will exclude in schema.
+        private _value = 0;
+        // basic property names will include in schema.
+        value = 1;
+        // invalid value types will exclude in schema.
+        join() {
+          return this._value + this.value;
+        }
+      }
+
+      const sock = socket('domain', {
+        // delete linked record 3 days later from last access.
+        expiry: 3 * 24 * 60 * 60 * 1e3,
+        factory() {
+          return new Schema();
+        }
+      });
+      // load data from indexeddb a little later.
+      const link: Schema = sock.link('path');
+      // save data to indexeddb, and sync data between all tabs.
+      link.value = 1;
+      sock.destroy();
+    });
+
+    it('communicate', () => {
+      interface Schema extends LocalPortObject {
+      }
+      class Schema {
+        get event() {
+          return this.__event;
+        }
+        version = 0;
+      }
+
+      const sock = port('version', {
+        life: 3,
+        factory() {
+          return new Schema();
+        }
+      });
+      const link: Schema = sock.link();
+      const VERSION = 1;
+      link.event.monitor('recv', ({newValue}) => {
+        if (newValue === VERSION) return;
+        if (newValue > VERSION) {
+          location.reload();
+        }
+        else {
+          link.version = VERSION;
+        }
+      });
+      link.version = VERSION;
+      sock.destroy();
+    });
+
+  });
+
+  describe('power-assert', function () {
+    it('assertion self-check', function (done) {
+      setTimeout(function () {
+        try {
+          console.log(assert(false === true), assert); // LOG: undefined, function powerAssert() { ... }
+        }
+        catch (e) {
+          done();
+          return;
+        }
+        throw new Error('WARNING!: assert function does not work.');
+      }, 1);
+    });
+
+  });
+
+});
