16107a9e2bc84ee77c5c99b1ff1504a4a57657ab
falsandtru <falsandtru@users.noreply.github.com>
2017-01-20 17:27:17 +0900

    refactor(global): remove unused workarounds

--- global.d.ts
+++ global.d.ts
@@ -4,14 +4,6 @@ import _assert from 'power-assert';
 declare global {
   export const assert: typeof _assert;
 
-  interface PromiseLike<T> {
-    _?: T;
-  }
-
-  interface Promise<T> {
-    _?: T;
-  }
-
   interface String {
     split(separator: string | RegExp, limit?: number): string[];
   }
