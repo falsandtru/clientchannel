9ae8fa12d8521cf91e68c6aa35bcead11d3648f5
falsandtru <falsandtru@users.noreply.github.com>
2017-03-28 23:14:15 +0900

    chore(config): update dependencies

--- global.d.ts
+++ global.d.ts
@@ -4,14 +4,6 @@ import _assert from 'power-assert';
 declare global {
   export const assert: typeof _assert;
 
-  interface String {
-    split(separator: string | RegExp, limit?: number): string[];
-  }
-
-  interface Array<T> {
-    split(separator: string | RegExp, limit?: number): T[];
-  }
-
   interface IDBDatabase {
     onclose: (ev: Event) => any;
   }
