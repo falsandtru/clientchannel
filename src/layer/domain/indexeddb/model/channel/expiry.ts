ffa3567860594b65c8a161181ce1a38dcf1c36f6
falsandtru <falsandtru@users.noreply.github.com>
2022-04-14 05:24:42 +0900

    Refactoring

--- src/layer/domain/indexeddb/model/channel/expiry.ts
+++ src/layer/domain/indexeddb/model/channel/expiry.ts
@@ -65,7 +65,7 @@ export class ExpiryStore<K extends string> {
         if (schedule === 0) return;
         if (!this.ownership.take('store', 10 * 1000)) return this.schedule(delay *= 2);
         delay = Math.max(Math.floor(delay / 1.5), delay);
-        const since = Date.now();
+        let count = 0;
         let retry = false;
         schedule = 0;
         return void this.store.cursor(null, ExpiryStoreSchema.expiry, 'next', 'readonly', (cursor, error) => {
@@ -76,7 +76,7 @@ export class ExpiryStore<K extends string> {
           const { key, expiry }: ExpiryRecord<K> = cursor.value;
           if (expiry > Date.now()) return void this.schedule(expiry - Date.now());
           if (!this.ownership.extend('store', 10 * 1000)) return void this.schedule(delay *= 2);
-          if (Date.now() > since + 3 * 1000) return void this.schedule(5 * 1000);
+          if (++count > 100) return void this.schedule(5 * 1000);
           schedule = 0;
           if (!this.ownership.take(`key:${key}`, 10 * 1000)) return retry = true, void cursor.continue();
           this.chan.has(key) || this.chan.meta(key).date === 0
