e016751c6848f3adf1ed87714b91555795462697
falsandtru <falsandtru@users.noreply.github.com>
2022-05-07 07:16:12 +0900

    Refactoring

--- src/layer/domain/indexeddb/model/channel.test.ts
+++ src/layer/domain/indexeddb/model/channel.test.ts
@@ -11,8 +11,8 @@ describe('Unit: layers/domain/indexeddb/model/channel', function () {
       destroy('test');
     });
 
-    interface CustomSocketValue extends ChannelStore.Value { }
-    class CustomSocketValue {
+    interface Value extends ChannelStore.Value { }
+    class Value {
       constructor(
         public value: number
       ) {
@@ -26,27 +26,27 @@ describe('Unit: layers/domain/indexeddb/model/channel', function () {
     });
 
     it('recent', async () => {
-      const chan = new ChannelStore<string, CustomSocketValue>('test', () => true, Infinity, Infinity);
+      const chan = new ChannelStore<string, Value>('test', () => true, Infinity, Infinity);
 
       assert.deepStrictEqual(await chan.recent(), []);
       await chan.recent();
-      chan.add(new ChannelStore.Record('a', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('a', new Value(0)));
       await new Promise(resolve => chan.events.save.once(['a', 'value', 'put'], resolve));
       assert.deepStrictEqual(await chan.recent(), ['a']);
-      chan.add(new ChannelStore.Record('b', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('b', new Value(0)));
       await new Promise(resolve => chan.events.save.once(['b', 'value', 'put'], resolve));
       assert.deepStrictEqual(await chan.recent(), ['b', 'a']);
       chan.destroy();
     });
 
     it('clean', async () => {
-      const chan = new ChannelStore<string, CustomSocketValue>('test', () => true, Infinity, Infinity);
+      const chan = new ChannelStore<string, Value>('test', () => true, Infinity, Infinity);
 
-      chan.add(new ChannelStore.Record('a', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('a', new Value(0)));
       assert.deepStrictEqual(await chan.recent(), ['a']);
       assert(chan.has('a') === true);
       chan.delete('a');
-      chan.add(new ChannelStore.Record('b', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('b', new Value(0)));
       await new Promise(resolve => chan.events.save.once(['a', '', 'delete'], resolve));
       assert.deepStrictEqual(await chan.recent(), ['b']);
       assert(chan.has('a') === false);
@@ -60,12 +60,12 @@ describe('Unit: layers/domain/indexeddb/model/channel', function () {
     });
 
     it('age', async () => {
-      const chan = new ChannelStore<string, CustomSocketValue>('test', () => true, Infinity, Infinity);
+      const chan = new ChannelStore<string, Value>('test', () => true, Infinity, Infinity);
 
-      chan.add(new ChannelStore.Record('a', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('a', new Value(0)));
       chan.expire('b', 100);
-      chan.add(new ChannelStore.Record('b', new CustomSocketValue(0)));
-      chan.add(new ChannelStore.Record('c', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('b', new Value(0)));
+      chan.add(new ChannelStore.Record('c', new Value(0)));
       await new Promise(resolve => chan.events.save.once(['b', '', 'delete'], resolve));
       assert.deepStrictEqual(await chan.recent(), ['c', 'a']);
       assert(chan.has('a') === true);
@@ -75,13 +75,13 @@ describe('Unit: layers/domain/indexeddb/model/channel', function () {
     });
 
     it('size', async () => {
-      const chan = new ChannelStore<string, CustomSocketValue>('test', () => true, Infinity, 2);
+      const chan = new ChannelStore<string, Value>('test', () => true, Infinity, 2);
 
-      chan.add(new ChannelStore.Record('b', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('b', new Value(0)));
       for (let until = Date.now() + 1; Date.now() < until;); // Wait
-      chan.add(new ChannelStore.Record('a', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('a', new Value(0)));
       for (let until = Date.now() + 1; Date.now() < until;); // Wait
-      chan.add(new ChannelStore.Record('c', new CustomSocketValue(0)));
+      chan.add(new ChannelStore.Record('c', new Value(0)));
       assert(chan.has('b') === true);
       assert(chan.has('a') === true);
       assert(chan.has('c') === true);
