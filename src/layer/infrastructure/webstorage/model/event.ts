758d2221332a8820f3007b4b09e19e668926160f
falsandtru <falsandtru@users.noreply.github.com>
2022-10-08 19:41:56 +0900

    Refactoring

--- src/layer/infrastructure/webstorage/model/event.ts
+++ src/layer/infrastructure/webstorage/model/event.ts
@@ -6,7 +6,7 @@ type Type = ['local' | 'session'] | ['local' | 'session', string];
 export const storageEventStream$ = new Observation<Type, StorageEvent, void>({ limit: Infinity });
 export const storageEventStream: Observer<Type, StorageEvent, void> = storageEventStream$;
 
-void self.addEventListener('storage', event => {
+self.addEventListener('storage', event => {
   switch (event.storageArea) {
     case localStorage:
       return void storageEventStream$.emit(event.key === null ? ['local'] : ['local', event.key], event);
